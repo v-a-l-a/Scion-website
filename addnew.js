a2b5ae09c3b567765b0eee2bd665ec4428087ba7
v-a-l-a <noora.sheridan@helsinki.fi>
2018-07-12 17:26:20 +0300

    Separated js, css and html

---
+++ addnew.js
@@ -0,0 +1,128 @@
+function Tier (tiername){
+  this.Tiername = tiername;
+}
+
+function StoreRaiderInfo(){
+  var raidersName, raidersMS, raidersOS, text;
+  offspecoptions = ["OS1", "OS2", "OS3", "OS4"];
+  offspecroles = ["melee DPS", "ranged DPS", "healer", "tank"];
+  raidersOS = "";
+  OScode = ""
+  raidersName = document.getElementById("AddedName").value;
+  if (document.getElementById("AddedRole1").checked) {
+    document.getElementById("OS1").checked=false;
+    if (document.getElementById("OS2").checked) {
+      raidersMS = "DPSt"
+    } else {
+    raidersMS = "DPSx";
+    }
+  } else if (document.getElementById("AddedRole2").checked) {
+    document.getElementById("OS2").checked=false;
+      if (document.getElementById("OS1").checked) {
+        raidersMS = "DPSt"
+      } else {
+      raidersMS = "DPSr"
+    }
+    } else if (document.getElementById("AddedRole3").checked) {
+      raidersMS = "healer"
+      document.getElementById("OS3").checked=false;
+    } else {
+      raidersMS = "tank";
+      document.getElementById("OS4").unchecked=false;
+    }
+    for (i = 0; i < 4; i++) {
+  if (document.getElementById(offspecoptions[i]).checked) {
+    raidersOS = raidersOS + offspecroles[i];
+    OScode = OScode + "1";
+  } else {
+      raidersOS = raidersOS;
+      OScode = OScode + "0";
+    }
+  }
+if (raidersOS.length==0) {
+  raidersOS = "none";
+  OScode = OScode + "1";
+} else {
+    raidersOS = raidersOS;
+    OScode = OScode + "0";
+  }
+  if (raidersName.length ==0) {
+    text = "Please fill the name";
+    } else {
+      var raiderobj = new Raider (raidersName, raidersMS, OScode);
+      var raiderJSON = JSON.stringify(raiderobj);
+    text = "Name: " + raidersName + "<br>" + "Role: " + raidersMS +
+    "<br>" + "Other roles: " + raidersOS + "<br>" + "With OS code of: " + OScode + "<br>" + "JSON string:" + raiderJSON;
+    document.getElementById("AddRaiderBox").reset();
+  }
+document.getElementById("StoredInfoAsText").innerHTML = text;
+}
+function StoreBossInfo(){
+  var bossName = document.getElementById("AddedBossName").value;
+  tankIDs = ["tanks1", "tanks2", "tanks3", "tanks4"];
+  var tankN = [];
+  for (i=0; i<4; i++){
+  if (document.getElementById(tankIDs[i]).checked){
+    tankAmount = i + 1
+    tankN.push(tankAmount);
+  }
+  }
+  if (tankN.length == 0) {
+  minTanks = 2;
+  maxTanks = 2;
+  } else {
+  minTanks = tankN[0];
+  tanksTicked = tankN.length-1;
+  maxTanks = tankN[tanksTicked];
+  }
+  healerIDs = ["healers3", "healers4", "healers5", "healers6"];
+  var healerN = [];
+  for (i=0; i<4; i++){
+  if (document.getElementById(healerIDs[i]).checked){
+    healerAmount = i + 3;
+    healerN.push(healerAmount);
+  }
+  }
+  if (healerN.length==0){
+  minHealers = 4;
+  maxHealers = 4;
+  } else {
+  minHealers = healerN[0];
+  healersTicked = healerN.length - 1;
+  maxHealers = healerN[healersTicked];
+  }
+  if (bossName.length==0){
+  text = "Please fill the fight name";
+  } else {
+  text = "Name: " + bossName + "<br>" + "Tanks: " + "min. " + minTanks + " max: " + maxTanks
+  + "<br>" + "Healers: " + "min. " + minHealers + " max: " + maxHealers;
+  var boss = new Boss(bossname, mintanks, maxtanks, minhealers, maxhealers, tier);
+  document.getElementById("AddFightBox").reset();
+  }
+  document.getElementById("BossInfoAsText").innerHTML=text;
+  }
+function hide(idText){
+  document.getElementById(idText).style.display="none";
+  document.getElementById(idText).reset();
+  document.getElementById("StoredInfoAsText").style.display="none";
+  document.getElementById("BossInfoAsText").style.display="none";
+  document.getElementById("tier").style.display="";
+}
+function toggleBox(idT){
+  var aBox = document.getElementById(idT);
+  if (aBox.style.display === "none") {
+    aBox.style.display = "block";
+  } else {
+    aBox.style.display = "none";
+  }
+}
+function addTier(){
+ document.getElementById("tierform").style.display="block";
+  document.getElementById("tier").style.display="none";
+}
+function storeTierInfo(){
+  var tiername = document.getElementById("tiern").value;
+  var tier = new Tier (tiername);
+  document.getElementById("tieradd").innerHTML += tier.Tiername;
+  hide('tierform');
+}
---
+++ tablecreation.js
@@ -0,0 +1,313 @@
+function otherrows(raiders, nofcolumns, row, rown){
+  for (i=0; i<nofcolumns; i++) {
+        var cell = row.insertCell(i);
+          if (i===0){
+            createCell1(cell, row, i, raiders, rown)
+      } else if (i===1) {
+        cell.innerHTML = raiderObjects[rown-1].roleN;
+      } else {
+          createCell(cell, rown, i)
+        }} i = rown
+}
+function createCell1(cell, row, i, raiders, rown){
+  RowId = row;
+  cell.innerHTML =
+  "<button class ='accDec' onclick='setAcc(this)' id=('accepted' + RowId)><i class='fa fa-check' style='color:DarkSeaGreen'></i></button>"+
+  "<button class ='accDec' onclick='setDec(this)' id=('declined' + RowId)><i class='fa fa-close' style='color:darkslategray'></i></button>"+
+  "<button id=('nameText' + RowId) class='RnameText'>"+raiderObjects[rown-1].nameN+"</button>";
+  cell.style.width="130px";
+}
+
+function createCell(cell, rown, i) {
+  cellid = "r" + [rown] + "c" + [i];
+  setdraftid = "setDraftStatus" + cellid;
+  cell.innerHTML =
+  "<button id="+setdraftid+" class='tbutton' onclick='setDraftStatus(this," + rown +", "+ i + ")'></button>" +
+  "<button class='sitButton' id=('setStandby' + cellid) onclick='setAsStandby(this,"+i+", "+rown+")'></button>";
+  document.getElementById(setdraftid).innerText = "-";
+}
+
+function setAsStandby(button, i, rown) {
+  var standbyTicked = button.parentElement.getElementsByClassName("tbutton")[0];
+  standbyTicked.style.backgroundColor = "darkslategray";
+  standbyTicked.innerText = "standby";
+  standbyTicked.style.color = "white";
+  var table = document.getElementById("draftTable");
+  table.rows[rown].cells[i].class ="standbyclass";
+  var cellnu = i;
+  counterTableShow(cellnu);
+}
+function setColour(varName, bcCol, textCol, borderCol) {
+  varName.style.backgroundColor = bcCol;
+  varName.style.border = borderCol;
+  varName.style.color = textCol;
+}
+function setAcc(button) {
+  var varName = button.parentElement.getElementsByClassName("RnameText")[0];
+  if (varName.style.backgroundColor == "darkseagreen") {
+    var bcCol = "white";
+    var textCol = "black";
+    var borderCol = "white";
+        button.parentElement.class = "notchosen";
+    setColour(varName, bcCol, textCol, borderCol)
+  } else {
+    var bcCol = "DarkSeaGreen";
+    var textCol = "white";
+    var borderCol = "DarkSeaGreen";
+        button.parentElement.class = "acces";
+    setColour(varName, bcCol, textCol, borderCol)
+  }}
+function setDec(button){
+  var varName = button.parentElement.getElementsByClassName("RnameText")[0];
+  if (varName.style.backgroundColor == "darkslategray") {
+    var bcCol = "white";
+    var textCol = "black";
+    var borderCol = "white";
+    button.parentElement.class = "notchosen";
+    setColour(varName, bcCol, textCol, borderCol)
+  } else {
+    var bcCol = "darkslategray";
+    var textCol = "white";
+    var borderCol = "darkslategray";
+    button.parentElement.class = "decl";
+    setColour(varName, bcCol, textCol, borderCol)
+  }}
+
+function createfirstrow(bosses, nofcolumns, row, rown){
+  for (i=0; i<nofcolumns; i++) {
+    var cell = row.insertCell(i);
+      if (i===0){
+        cell.innerHTML = "";
+      } else if (i===1){
+        cell.innerHTML = "R";
+      } else
+    cell.innerHTML = bosses[i-2].Bname;
+    cell.style.textAlign="center";
+    cell.style.height="50px";
+  } i = rown
+}
+function createTable(bosses){
+    var table = document.getElementById("draftTable");
+    var raiders = ["Vala", "Haseo", "Ilnezhara"];
+    var nofrows = raiderObjects.length + 1;
+    var nofcolumns = bosses.length + 2;
+    for (i=0; i<nofrows; i++){
+      var rown = i
+      var row = table.insertRow(i);
+      if (i === 0) {
+        createfirstrow(bosses, nofcolumns, row, rown)
+      } else {
+        otherrows(raiders, nofcolumns, row, rown)
+      }
+    }}
+function setDraftStatus(button, rown, i){
+    var cellnu = i;
+      var table = document.getElementById("draftTable");
+      table.rows[rown].cells[i].class ="notstandby";
+      counterTableShow(cellnu);
+      var vala = raiderObjects[rown-1].offroleN;
+      var raiderStatus = button
+      raiderStatus.style.color = "black";
+      if (raiderStatus.innerText === "DPS") {
+        if ((vala.charAt(3)) === "1") {
+          raiderStatus.innerText = "tank";
+          raiderStatus.style.backgroundColor = "lightgray";
+        } else if ((vala.charAt(2)) === "1") {
+          raiderStatus.innerText = "healer";
+          raiderStatus.style.backgroundColor = "DarkSeaGreen";
+        } else {
+            raiderStatus.innerText = "-";
+            raiderStatus.style.backgroundColor = "white";
+      }} else if (raiderStatus.innerText === "tank") {
+            if ((vala.charAt(2)) === "1") {
+            raiderStatus.innerText = "healer";
+            raiderStatus.style.backgroundColor = "DarkSeaGreen";
+          } else {
+          raiderStatus.innerText = "-";
+          raiderStatus.style.backgroundColor = "white";
+      }} else if (raiderStatus.innerText === "-") {
+      raiderStatus.innerText = "x";
+      raiderStatus.style.backgroundColor = "lavender";
+    } else if (raiderStatus.innerText === "x") {
+        if ((vala.charAt(0)) === "1" || (vala.charAt(1)) === "1") {
+        raiderStatus.innerText = "DPS";
+        raiderStatus.style.backgroundColor = "lightblue";
+      } else if ((vala.charAt(3)) === "1") {
+        raiderStatus.innerText = "tank";
+        raiderStatus.style.backgroundColor = "lightgray";
+      } else if ((vala.charAt(2)) === "1") {
+        raiderStatus.innerText = "healer";
+        raiderStatus.style.backgroundColor = "DarkSeaGreen";
+    } else {
+        raiderStatus.innerText = "-";
+        raiderStatus.style.backgroundColor = "white";
+  }} else {
+        raiderStatus.innerText = "-";
+        raiderStatus.style.backgroundColor = "white";
+    }}
+function showDeclined(){
+  var table = document.getElementById("draftTable");
+  var nofr = table.rows.length;
+  for (i=1; i<nofr; i++){
+      table.rows[i].style.display = "";
+    }
+    document.getElementById("numAcc").innerText = "";
+}
+function sortTable(){
+  var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementById("draftTable");
+  switching = true;
+  while (switching) {
+    switching = false;
+    rows = table.getElementsByTagName("TR");
+    for (i=1; i< (rows.length -1); i++) {
+      shouldSwitch = false;
+      x = rows[i].getElementsByTagName("TD")[1];
+      y = rows[i + 1].getElementsByTagName("TD")[1];
+      if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+        shouldSwitch = true;
+        break;
+      }
+    } if (shouldSwitch) {
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
+}
+function separateHealers(){
+  var table, rows, columns, i, x, y, z;
+  table = document.getElementById("draftTable");
+  rows = table.getElementsByTagName("TR");
+  columns = document.getElementById('draftTable').rows[0].cells.length;
+  healcol = "#c3a2a2";
+    for (i=1; i< (rows.length -1); i++) {
+      numb = i;
+      x = rows[i].getElementsByTagName("TD")[1];
+      y = rows[i + 1].getElementsByTagName("TD")[1];
+      if (x.innerHTML ==="tank" && y.innerHTML ==="healer") {
+        var firstHealer = i + 1;
+        for (i=1; i<columns; i++) {
+          z = rows[firstHealer].getElementsByTagName("TD")[i];
+          z.style.backgroundColor= healcol;
+        } i = numb;
+      } else if (x.innerHTML ==="healer" && y.innerHTML ==="DPSx") {
+      var lastHealer = i;
+      for (i=1; i<columns; i++) {
+        z = rows[numb].getElementsByTagName("TD")[i];
+        z.style.backgroundColor= healcol;
+      } i = numb;
+    } else if (x.innerHTML === "healer"){
+      for (i=1; i<columns; i++) {
+        z = rows[numb].getElementsByTagName("TD")[i];
+        z.style.backgroundColor= healcol;
+    } i = numb;
+  }
+}}
+function hideRoleColumn(){
+  var table = document.getElementById("draftTable");
+  var nofr = table.rows.length;
+  for (i=0; i<nofr; i++){
+    table.rows[i].cells[1].style.display = "none";
+}}
+function tableLoad(){
+  createTable(bossObjectArray);
+  sortTable();
+  separateHealers();
+  hideRoleColumn();
+  createCountertable();
+  createTierList();
+}
+function showBossColumn(saveBosses){
+  var table = document.getElementById("draftTable");
+  var nofr = table.rows.length;
+  for (i=0; i<bossObjectArray.length; i++) {
+    bossi = bossObjectArray[i];
+    cell = i + 2;
+  if (saveBosses.includes(bossi.Bname) == false){
+  hideColumn(cell);
+  i = cell -2;
+  }}}
+function hideColumn(cell){
+  var table = document.getElementById("draftTable");
+  var nofr = table.rows.length;
+  for (i=0; i<nofr; i++){
+    table.rows[i].cells[cell].style.display = "none";
+}}
+
+function hideDeclined(){
+  var table = document.getElementById("draftTable");
+  var nofr = table.rows.length;
+  var numAcce = 0;
+  for (i=1; i<nofr; i++){
+    var cellstatus = table.rows[i].cells[0];
+    if (cellstatus.class === "decl") {
+      table.rows[i].style.display = "none";
+    } else if(cellstatus.class === "acces") {
+      numAcce = numAcce + 1;
+    }  }
+  document.getElementById("numAcc").innerText = "Total Accepted: " + numAcce;
+}
+function createCountertable(){
+  var table = document.getElementById('draftTable');
+  var nofr = table.rows.length;
+  row = table.insertRow(nofr);
+  columns = table.rows[0].cells.length;
+  for (i=0; i<columns; i++) {
+      var cell = row.insertCell(i);
+      cell.class = "counttable"
+    if (i===0) {
+    cell.innerText = "n. to sit:";
+  } else if (i===1) {
+  cell.style.display = "none"; }
+  else {
+  var clicks = 0;
+  cell.innerHTML = clicks;
+}}}
+function counterTableShow(cellnu){
+  var table = document.getElementById("draftTable");
+  var nofr = table.rows.length;
+  var numSby = 0;
+  for (i=1; i<nofr; i++){
+    var cellstatus = table.rows[i].cells[cellnu];
+    if (cellstatus.class === "standbyclass") {
+      numSby = numSby + 1;
+    }}
+  var ctable = document.getElementById("countertable");
+  totnum = document.getElementById("numAcc").innerHTML;
+  ctable.rows[0].cells[cellnu-1].innerHTML = totnum + " - " + numSby;
+  }
+function createTierList(){
+  var noftiers = 3;
+  for (i=0; i<noftiers; i++){
+    buttonid = "b" + i;
+    document.getElementById("tierSelection").innerHTML +=
+    "<button id="+buttonid+" onclick='showTierBosses(this)'>"+i+"</button>"
+  }
+}
+function showTierBosses(button){
+  i = button.innerHTML;
+  tierchosen = tiers[i];
+  tiern = "Tier"+i;
+  var tierchoices = document.getElementById("tiersBosses");
+  tierchoices.innerHTML ="<br>";
+  bossesChosen = [];
+  for (i=0; i<bossObjectArray.length; i++){
+    if (bossObjectArray[i].Btier === tiern){
+      checkboxname = "boss" +i;
+      idname = bossObjectArray[i].Bname;
+    tierchoices.innerHTML +=
+    "<input id="+idname+" class='bossCheckBox' type = 'checkbox' name="+checkboxname+">"+idname + " ";
+    }}
+  tierchoices.innerHTML += "<br>" + "<br>" + "<button onclick ='selectBosses()'>Select</button>"
+}
+function selectBosses(){
+  saveBosses = [];
+  bossesChosen = document.getElementsByClassName("bossCheckBox");
+  for (i=0; i<bossesChosen.length; i++){
+    if (bossesChosen[i].checked) {
+      bossnam = bossesChosen[i].id;
+      saveBosses.push(bossnam);
+  }
+} if (saveBosses.length > 0) {
+  showBossColumn(saveBosses);
+}}
