e5651b6891dcc5221b48baa0cc02ab2353323641
Jack Sheridan <jack.sheridan@helsinki.fi>
2018-11-23 22:02:10 +0200

    fix apply page

--- src/main/java/com/js/paradigm/ApplyController.java
+++ src/main/java/com/js/paradigm/ApplyController.java
@@ -1,4 +1,14 @@
-package com.js.paradigm.model;
+package com.js.paradigm;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class ApplyController {
+
+    @RequestMapping(value = "/apply", produces = "text/html")
+    public String displayApply(){
+        return "apply";
+    }
 }
