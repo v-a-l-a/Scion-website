bef3c18664d2a881ef5b1f9c925d764c5e4e9cb2
Jack Sheridan <jack.sheridan@helsinki.fi>
2018-11-24 11:10:11 +0200

    Update mongodb config

--- src/main/java/com/js/paradigm/RaiderController.java
+++ src/main/java/com/js/paradigm/RaiderController.java
@@ -1,39 +1,39 @@
-//package com.js.paradigm;
-//
-//import com.js.paradigm.model.RaiderList;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import com.js.paradigm.data.RaiderRepository;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//
-///**
-// *
-// * @author sjack
-// */
-//@Controller
-//public class RaiderController {
-//
-//    @Autowired
-//    private RaiderRepository repo;
-//
-//
-//
-//
+package com.js.paradigm;
+
+import com.js.paradigm.model.RaiderList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.js.paradigm.data.RaiderRepository;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *
+ * @author sjack
+ */
+@Controller
+public class RaiderController {
+
+    @Autowired
+    private RaiderRepository repo;
+
+
+
+
+    @ResponseBody
+    @GetMapping(value = "/raiders/all", produces = "application/json")
+    public RaiderList listRaiders() {
+
+        RaiderList raiders = new RaiderList(repo.findAll());
+        System.out.println("Raiders: " + raiders);
+        return raiders;
+    }
+
 //    @ResponseBody
-//    @GetMapping(value = "/raiders/all", produces = "application/json")
-//    public RaiderList listRaiders() {
+//    @GetMapping(value = "/raiders/{id}")
+//    public Raider getRaiderById(@RequestParam(name="id", required = true) String id){
+//        return repo.findByID(id);
 //
-//        RaiderList raiders = new RaiderList(repo.findAll());
-//        System.out.println("Raiders: " + raiders);
-//        return raiders;
 //    }
-//
-////    @ResponseBody
-////    @GetMapping(value = "/raiders/{id}")
-////    public Raider getRaiderById(@RequestParam(name="id", required = true) String id){
-////        return repo.findByID(id);
-////
-////    }
-//
-//}
+
+}
--- src/main/java/com/js/paradigm/data/RaiderRepository.java
+++ src/main/java/com/js/paradigm/data/RaiderRepository.java
@@ -1,20 +1,20 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package com.js.paradigm.data;
-//
-//import com.js.paradigm.model.Raider;
-//import java.util.List;
-//import org.springframework.data.mongodb.repository.MongoRepository;
-//
-///**
-// *
-// * @author jack
-// */
-//public interface RaiderRepository extends MongoRepository<Raider, String>{
-//
-//    public Raider findByName(String name);
-//    public List<Raider> findByRole(String role);
-//}
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.js.paradigm.data;
+
+import com.js.paradigm.model.Raider;
+import java.util.List;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ *
+ * @author jack
+ */
+public interface RaiderRepository extends MongoRepository<Raider, String>{
+
+    public Raider findByName(String name);
+    public List<Raider> findByRole(String role);
+}
